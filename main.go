7fd23ca26b209a31acab3dc5c03e6ee14fa493ec
rguthriemsft <rguthrie@Microsoft.com>
2017-02-15 14:02:22 -0800

    dmeo 2

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<font size=\"8\">Hello Damien!!</font>")
+	io.WriteString(w, "<font size=\"8\">Hello Damien2!!</font>")
 }
 
 func main() {
